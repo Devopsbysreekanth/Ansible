be155f7e80ad5ada98b781cc0985a7ca6cbd8599
SreekanthMadhava75 <sreekanth.madhava75@gmail.com>
2024-05-12 20:44:42 +0530

    robo-shop ansible

---
+++ roboshop-ansible/create-server.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+NAMES=$@
+INSTANCE_TYPE="t2.micro"
+IMAGE_ID=ami-0f3c7d07486cad139
+SECURITY_GROUP_ID=sg-0285ea5029e3b42a8
+DOMAIN_NAME=devopsbysreekanth.online
+
+
+
+for i in $@
+do
+
+    # Check if instance already exists
+    if aws ec2 describe-instances --filters "Name=tag:Name,Values=$i" --query "Reservations[].Instances[].InstanceId" --output text | grep -q '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}'; then
+        echo "$i instance already exists, skipping..."
+        continue
+    fi
+    echo "creating $i instance"
+
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z1003307YHLZ8XIIWXDL --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+done
